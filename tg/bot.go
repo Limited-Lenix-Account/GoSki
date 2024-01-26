887edd87536efd9efe1be6f4ab803c908e7c6896
Lenix Not Linux <113266208+Limited-Lenix-Account@users.noreply.github.com>
2024-01-25 22:46:59 -0700

    added start / edit functions

--- tg/bot.go
+++ tg/bot.go
@@ -1,5 +1,47 @@
 package tg
 
-func StartBot() {
+import (
+	"fmt"
+	"log"
 
+	tg "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+const (
+	chatID = int64(1585424137)
+	botKey = "6582079213:AAE43uCNLr2pUWIgBB9zhYqlOQCbwFzpLEk"
+)
+
+func StartBot() *tg.BotAPI {
+
+	bot, err := tg.NewBotAPI(botKey)
+	if err != nil {
+		fmt.Printf("Error Creating Telegram Bot %s", err)
+	}
+
+	return bot
+
+}
+
+func SendMessage(bot *tg.BotAPI, finalMessage string) int {
+
+	msg := tg.NewMessage(chatID, finalMessage)
+	msg.ParseMode = tg.ModeMarkdownV2
+	// Send the message
+	sentMessage, err := bot.Send(msg)
+	if err != nil {
+		log.Panic(err)
+	}
+	return sentMessage.MessageID
+
+}
+
+func EditMessage(bot *tg.BotAPI, editMessage string, messageID int) {
+	editMsg := tg.NewEditMessageText(chatID, messageID, editMessage)
+	editMsg.ParseMode = tg.ModeMarkdownV2
+
+	_, err := bot.Send(editMsg)
+	if err != nil {
+		log.Panic(err)
+	}
 }
