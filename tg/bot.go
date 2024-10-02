df07d48c98db3de825376ad120176794c1f7d8af
Lenix Not Linux <113266208+Limited-Lenix-Account@users.noreply.github.com>
2024-10-02 16:39:34 -0600

    uses env for variables

--- tg/bot.go
+++ tg/bot.go
@@ -3,16 +3,32 @@ package tg
 import (
 	"fmt"
 	"log"
+	"os"
+	"strconv"
 
 	tg "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
 )
 
-const (
-	chatID = int64(11111) // telegram chat channel ID here
-	botKey = ""           // telegram bot key here
+var (
+	chatID int64  // telegram chat channel ID here
+	botKey string // telegram bot key here
+
 )
+
+func init() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	botKey = os.Getenv("BOT_ID")
+	chatIDInt, _ := strconv.Atoi(os.Getenv("CHAT_ID"))
+	chatID = int64(chatIDInt)
+}
 
 func StartBot() *tg.BotAPI {
+	fmt.Println(botKey)
 	fmt.Println("Starting TG Bot...")
 
 	bot, err := tg.NewBotAPI(botKey)
